a2cce811ff3e2586235c442693935ae36fc7a1e5
mohamed musthafa <imusthafa7@gmail.com>
2022-01-27 11:25:32 +0400

    initial commit

---
+++ src/Printnum.java
@@ -0,0 +1,16 @@
+
+public class Printnum {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		  String nam = "java";
+		 
+				
+			    System.out.println("the world is"+nam);
+ 
+	}
+	
+
+}
+
+ 
